5acd8eaafdf471d3f0e191b25a79b1ae399a5657
onetwothird <angelitodecatoriaa@gmail.com>
2026-06-30 22:11:40 +0800

    add a magnetic buttons

---
+++ app/src/components/Magnetic.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { motion } from 'framer-motion';
+import { useRef, useState, MouseEvent } from 'react';
+
+export default function Magnetic({ children }: { children: React.ReactNode }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouse = (e: MouseEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = e;
+    const boundingRect = ref.current?.getBoundingClientRect();
+    if (boundingRect) {
+      const { width, height, top, left } = boundingRect;
+      const x = clientX - (left + width / 2);
+      const y = clientY - (top + height / 2);
+      setPosition({ x, y });
+    }
+  };
+
+  const reset = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = position;
+  return (
+    <motion.div
+      style={{ position: "relative", zIndex: 50 }}
+      ref={ref}
+      onMouseMove={handleMouse}
+      onMouseLeave={reset}
+      animate={{ x: x * 0.4, y: y * 0.4 }}
+      transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
