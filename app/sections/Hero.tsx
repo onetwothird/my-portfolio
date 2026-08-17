bababebf97c3699a780215f2729bc1fbad7418dd
onetwothird <angelitodecatoriaa@gmail.com>
2026-08-17 17:01:59 +0800

    fix(hero): update image reference to new cover photo asset

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
         className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[200vw] h-[95vh] pointer-events-none z-0"
       >
         <Image 
-          src="/img/cover1.png" 
+          src="/img/cover-photos.png" 
           alt="Angelito Portrait" 
           fill 
           className="object-contain object-bottom" 
