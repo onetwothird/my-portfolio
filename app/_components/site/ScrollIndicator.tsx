86616a9eceb08bd7ca18f6d35c8a86bb949a2384
onetwothird <angelitodecatoriaa@gmail.com>
2026-09-06 02:02:24 +0800

    chore: move scroll indicator

--- app/_components/site/ScrollIndicator.tsx
+++ app/_components/site/ScrollIndicator.tsx
@@ -15,14 +15,10 @@ export default function ScrollIndicator() {
   const xPosition = useTransform(springProgress, [0, 1], ["0vw", "95vw"]);
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-99999 pointer-events-none">
-      <motion.div
-        className="h-1.5 bg-gray-800 dark:bg-gray-700 origin-left"
-        style={{ scaleX: springProgress }}
-      />
+    <div className="fixed bottom-0 left-0 right-0 z-50 pointer-events-none">
       
       <motion.div
-        className="absolute top-0 origin-bottom"
+        className="absolute bottom-1.5 origin-bottom"
         style={{ x: xPosition }}
         animate={{ 
           rotate: [-12, 12, -12],
@@ -43,6 +39,12 @@ export default function ScrollIndicator() {
           priority 
         />
       </motion.div>
+
+      <motion.div
+        className="h-1.5 bg-gray-800 dark:bg-gray-700 origin-left w-full"
+        style={{ scaleX: springProgress }}
+      />
+      
     </div>
   );
 }
